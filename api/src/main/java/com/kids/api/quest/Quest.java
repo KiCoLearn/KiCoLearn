d32b767027967064c61c1a9afb7bae5872e826c2
이진영 <ejnyg0613@naver.com>
2020-10-28 17:24:23 +0900

    S03P31A506-31 : Add quest function

---
+++ api/src/main/java/com/kids/api/quest/Quest.java
@@ -0,0 +1,18 @@
+package com.kids.api.quest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quest {
+    private int qusestNo;
+    private String name;
+    private int reward;
+    private String description;
+    private int parentId;
+    private boolean deleted;
+
+}
