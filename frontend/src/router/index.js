0c6033b7e528ffc36ab2eebaf6ad59006b4020b3
Hyun <yodg3456@naver.com>
2020-10-30 03:14:53 +0900

    S03P31A506-11 : Modify Signup page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,41 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-//import Home from '../views/Home.vue';
-import Sign from '../views/Sign.vue';
-
+//account
+import Sign from '@/views/account/Sign';
+import Main from '@/views/account/Main';
+import Kidsconnect from '@/views/account/Kidsconnect';
+import Kidslogin from '@/views/account/Kidslogin';
+//kids
+import Kidsmain from '@/views/Kids/Kidsmain';
 Vue.use(VueRouter);
 
 const routes = [
     {
+        path: '/',
+        name: 'main',
+        component:Main
+
+    },
+    {
         path: '/sign',
         name: 'Sign',
         component:Sign
-    }
+    },
+    {
+        path: '/kidsconnect',
+        name: 'Kidsconnect',
+        component:Kidsconnect
+    },
+    {
+        path: '/kidslogin',
+        name: 'Kidslogin',
+        component:Kidslogin
+    },
+    {
+        path: '/kidsmain',
+        name: 'Kidsmain',
+        component:Kidsmain
+    },
 ];
 
 const router = new VueRouter({
---
+++ frontend/src/views/account/Sign.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sign">
+        <h3>회원가입 </h3>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        kakaoLogin() {
+            window.Kakao.Auth.login({
+                scope: 'profile, account_email',
+                success: this.GetMe,
+            });
+        },
+    },
+    
+};
+</script>
+<style scoped>
+
+</style>
