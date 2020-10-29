160df2a5ee791ea0ecd5dba6f84115e91c13e283
김형준 <gudwns026@naver.com>
2020-10-29 14:49:41 +0900

    S03P31A506-37 : Fix Quiz CRUD

--- api/src/main/java/com/kids/api/quiz/QuizController.java
+++ api/src/main/java/com/kids/api/quiz/QuizController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ExampleProperty;
 
 @RestController
 @RequestMapping("/quiz")
@@ -27,7 +31,7 @@ public class QuizController {
     QuizService qService;
 
     @GetMapping("/")
-    @ApiOperation(value = "퀴즈 목록을 불러온다.")
+    @ApiOperation(value = "퀴즈 목록 조회")
     private ResponseEntity<Map<String, Object>> quizList() {
         ResponseEntity<Map<String, Object>> result = null;
         try {
@@ -40,7 +44,7 @@ public class QuizController {
     }
 
     @GetMapping("/{quizNo}")
-    @ApiOperation(value = "퀴즈번호로 퀴즈정보를 조회한다.")
+    @ApiOperation(value = "퀴즈번호로 퀴즈정보 조회")
     private ResponseEntity<Map<String, Object>> selectQuiz(@PathVariable int quizNo) {
         ResponseEntity<Map<String, Object>> result = null;
         try {
@@ -53,18 +57,46 @@ public class QuizController {
     }
 
     @PostMapping("/create")
-    @ApiOperation(value = "새로운 퀴즈를 생성한다.")
-    private ResponseEntity<Map<String, Object>> createQuiz(@RequestBody Map<String, Object> data) {
+    @ApiOperation(value = "새로운 퀴즈를 생성")
+    @ApiImplicitParams({
+        @ApiImplicitParam(
+            name = "quizInfo",
+            examples = @io.swagger.annotations.Example(
+                                                       
+                value = {
+                    @ExampleProperty(value = "{'property': 'test'}", mediaType = "application/json")
+                })) 
+    })
+    private ResponseEntity<Map<String, Object>> createQuiz(@RequestBody Quiz quiz) {
         ResponseEntity<Map<String, Object>> result = null;
-        try {
-            String question = (String) data.get("question");
-            boolean answer = (boolean) data.get("answer");
-            String description = (String) data.get("description");
-            String category = (String) data.get("category");
-            Quiz quiz = new Quiz(question, description, answer, category);
+        try {           
             result = handleSuccess(qService.create(quiz));
         } catch (Exception e) {
-            // TODO: handle exception
+            result = handleException(e);
+        }
+        return null;
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "퀴즈의 정보를 수정")
+    private ResponseEntity<Map<String, Object>> updateQuiz(@RequestBody Quiz quiz) {
+        ResponseEntity<Map<String, Object>> result = null;
+        try {            
+            result = handleSuccess(qService.update(quiz));
+        } catch (Exception e) {
+            result = handleException(e);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/delete/{quizNo}")
+    @ApiOperation(value = "퀴즈 삭제")
+    private ResponseEntity<Map<String, Object>> deleteQuiz(@PathVariable int quizNo) {
+        ResponseEntity<Map<String, Object>> result = null;
+        try {
+            result = handleSuccess(qService.delete(quizNo));
+        } catch (Exception e) {
+            result = handleException(e);
         }
         return null;
     }
@@ -89,8 +121,4 @@ public class QuizController {
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // private ResponseEntity<Map<String, Object>> template(){
-    // ResponseEntity<Map<String, Object>> result = null;
-    // return null;
-    // }
 }
