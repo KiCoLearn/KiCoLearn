205587a01a13cd0443af788be3bec0446d1f62bc
김형준 <gudwns026@naver.com>
2020-11-06 17:39:10 +0900

    S03P31A506-46 : Add item rgist dialog

--- frontend/src/components/items/AddItem.vue
+++ frontend/src/components/items/AddItem.vue
@@ -1,5 +1,62 @@
 <template>
-    <v-container>
+    <v-row justify="center">
+        <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="360"
+        >            
+            <v-card>
+                <v-card-title class="headline">                    
+                    아이템 등록
+                </v-card-title>
+                <template>
+                    <v-form
+                        id="form"
+                        ref="form"
+                        v-model="valid"
+                        lazy-validation
+                    >
+                        <v-text-field
+                            id="text"
+                            v-model="name"
+                            label="이름"
+                            required                            
+                        />
+                        <v-text-field
+                            id="text"
+                            v-model="price"
+                            label="가격"
+                            required                            
+                        />
+                        <v-text-field
+                            id="text"
+                            v-model="description"
+                            label="설명"
+                            required                            
+                        />                        
+                    </v-form>
+                </template>
+                <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="handleAddItem"
+                    >
+                        등록
+                    </v-btn>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="handleDialog"
+                    >
+                        취소
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+    <!-- <v-container>
         <h1>파일 업로더</h1>
         <input 
             id="file-selector" 
@@ -14,19 +71,48 @@
         >
             업로드
         </v-btn>
-    </v-container>
+    </v-container> -->
 </template>
 
 <script>
 import AWS from 'aws-sdk';
+import axios from 'axios';
+
 
 export default {
+    name:'AddItem',
+    props:{
+        dialog:{
+            type:Boolean
+        },
+    },
+    
     data(){
         return {
-            file:null,            
+            file:null,
+            name:null,
+            price:null,
+            description:null,
+            parentId:0,             
         };
     },
     methods: {
+        handleAddItem(){
+            axios.post(process.env.VUE_APP_API_URL+'/api/store/item/regist', {
+                description: this.description,    
+                name: this.name,
+                parentId: this.parentId,
+                price: this.price
+            })
+                .then(()=>{
+                    alert('등록되었습니다!');
+                    this.handleDialog();
+                    window.location.reload();
+                });
+        },
+        handleDialog(){
+            this.$emit('handle');
+        },
         handleFileUpload(){            
             this.file = this.$refs.file.files[0];
             console.log(this.file, '파일이 업로드 되었습니다.');
@@ -61,3 +147,13 @@ export default {
     }
 };
 </script>
+
+<style>
+#form{
+    padding:0.4rem;    
+}
+#text{
+    margin: 0.2rem;
+    width: 300px;
+}
+</style>
