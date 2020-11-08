f9980f5a4f0917180c1501816d7031a0e12b70a7
이진영 <ejnyg0613@naver.com>
2020-11-08 21:29:57 +0900

    S03P31A506-57 : Add time-zone (Asia/Seoul)

--- api/src/main/java/com/kids/api/ApiApplication.java
+++ api/src/main/java/com/kids/api/ApiApplication.java
@@ -1,13 +1,21 @@
 package com.kids.api;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
