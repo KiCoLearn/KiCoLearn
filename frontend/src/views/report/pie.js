c57a6e5d0ecf122307ff3a923c3a5f250dae6fed
yodg3456 <yodg3456@naver.com>
2020-11-12 02:43:27 +0900

    S03P31A506-78 : update chart data

--- frontend/src/views/report/pie.js
+++ frontend/src/views/report/pie.js
@@ -1,15 +1,18 @@
 
-import { Doughnut } from 'vue-chartjs';
+import { Doughnut, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
 export default {
     extends: Doughnut,
-    props: ['data', 'options'],
+    mixins:[reactiveProp],
+    props: ['options'],
     mounted() {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
-        this.renderChart(this.data, {
-            borderWidth: '10px',
-            hoverBackgroundColor: 'red',
-            hoverBorderWidth: '10px'
-        });
+        // this.renderChart(this.chartData, {
+        //     borderWidth: '10px',
+        //     hoverBackgroundColor: 'red',
+        //     hoverBorderWidth: '10px'
+        // });
+        this.renderChart(this.chartData, this.options);
     }
 };
