cb0f54d0f339449118c04a9868d21c56b7faef06
이진영 <ejnyg0613@naver.com>
2020-11-13 21:39:31 +0900

    S03P31A506-94 : Add handle quiz solving

--- api/src/main/java/com/kids/api/money/Budget.java
+++ api/src/main/java/com/kids/api/money/Budget.java
@@ -5,10 +5,12 @@ import java.sql.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class Budget {
     private String contents;
     private int amount;
