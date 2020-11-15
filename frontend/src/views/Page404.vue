80ec7239226a8087c3f293f326267b470438e938
leejinyoung <ejnyg0613@naver.com>
2020-11-15 21:35:50 +0900

    S03P31A506-111 : Update error 404 page

--- frontend/src/views/Page404.vue
+++ frontend/src/views/Page404.vue
@@ -2,14 +2,14 @@
     <div class="error2">
         <img
             src="@/assets/logo.png"
-            width="200px"
+            width="300px"
         >
         <br>
         <h2>잘못된 페이지 접근입니다.</h2>
         <br>
         <div class="nav">
             <button
-                class="head"
+                class="btn head"
                 @click="goHome"
             >
                 <b>메인으로 돌아가기</b>
@@ -21,7 +21,7 @@
 <script>
 export default {
     name: 'Error',
-    methods :{
+    methods : {
         goHome(){
             this.$router.push({name:'Main'});
         }
@@ -46,7 +46,6 @@ export default {
     justify-content: flex-end;
     margin-bottom: 20px;
   }
-
 
   .head{
     background:#fb8c00;
